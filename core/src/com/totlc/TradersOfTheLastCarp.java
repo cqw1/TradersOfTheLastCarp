fd22731b7a4192f57c01f1ae51c75f253e63b93d
cqw1 <cqw1@rice.edu>
2017-01-11 01:22:09 -0600

    walking dummy on black screen LOL. dummy isn't reflected and also is constantly walking right now

--- core/src/com/totlc/TradersOfTheLastCarp.java
+++ core/src/com/totlc/TradersOfTheLastCarp.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
@@ -21,6 +24,10 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 	Texture characterImg;
 	private OrthographicCamera camera;
 
+    TextureAtlas dummyTextureAtlas;
+    Animation<TextureRegion> animation;
+    float elapsedTime;
+
 	private Rectangle character;
 	
 	@Override
@@ -34,9 +41,12 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 		characterImg = new Texture(Gdx.files.internal("derpcharizard.png"));
 		character = new Rectangle();
 
+        dummyTextureAtlas = new TextureAtlas(Gdx.files.internal("dummy_spritesheet/dummy_spritesheet.atlas"));
+        animation = new Animation(1/15f, dummyTextureAtlas.getRegions());
+
 		// Dimension ratio is 1:2; width:height
-		character.width = 50;
-		character.height = character.width * 2;
+		character.width = 128;
+		character.height = character.width;
 
 		// Config dimensions are 1024 x 512 - see DesktopLauncher.java.
 		character.x = CONFIG_WIDTH / 2 - character.width / 2;
@@ -44,12 +54,17 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 	}
 
 	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
 	public void render() {
-		// Clear the screen with a red color. The
+		// Clear the screen with a white color. The
 		// arguments to glClearColor are the red, green
 		// blue and alpha component in the range [0,1]
 		// of the color to be used to clear the screen.
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		// Tell the camera to update its matrices.
@@ -58,7 +73,9 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 		// Begin a new batch. Draw character image.
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(characterImg, character.x, character.y);
+		//batch.draw(characterImg, character.x, character.y);
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        batch.draw(animation.getKeyFrame(elapsedTime, true), character.x, character.y);
 		batch.end();
 
 		// Process user input.
@@ -98,5 +115,6 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 	public void dispose() {
 		batch.dispose();
 		characterImg.dispose();
+        dummyTextureAtlas.dispose();
 	}
 }
