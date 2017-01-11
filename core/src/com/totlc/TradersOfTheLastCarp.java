5ab0d53f60f4ecb24a75135526c9d7117d299a7b
cqw1 <cqw1@rice.edu>
2017-01-11 13:26:51 -0600

    created dummy actor. no longer walks or has animation

--- core/src/com/totlc/TradersOfTheLastCarp.java
+++ core/src/com/totlc/TradersOfTheLastCarp.java
@@ -2,45 +2,44 @@ package com.totlc;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 
 public class TradersOfTheLastCarp extends ApplicationAdapter {
 	public static int CONFIG_WIDTH = 1024;
 	public static int CONFIG_HEIGHT = 512;
 
+	/*
 	SpriteBatch batch;
 	Texture characterImg;
-	private OrthographicCamera camera;
+
+	private Rectangle character;
 
     TextureAtlas dummyTextureAtlas;
     Animation<TextureRegion> animation;
     float elapsedTime;
+	*/
 
-	private Rectangle character;
-	
+	private OrthographicCamera camera;
+
+	Stage stage;
+
+
 	@Override
 	public void create() {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, CONFIG_WIDTH, CONFIG_HEIGHT);
 
-		batch = new SpriteBatch();
+		//batch = new SpriteBatch();
 
-		//derpCharizard = new Texture("derpcharizard.png");
-		characterImg = new Texture(Gdx.files.internal("derpcharizard.png"));
-		character = new Rectangle();
+		stage = new Stage();
 
+		Dummy dummy = new Dummy();
+		stage.addActor(dummy);
+
+		/*
         dummyTextureAtlas = new TextureAtlas(Gdx.files.internal("dummy_spritesheet/dummy_spritesheet.atlas"));
         animation = new Animation(1/12f, dummyTextureAtlas.getRegions());
 
@@ -51,6 +50,7 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 		// Config dimensions are 1024 x 512 - see DesktopLauncher.java.
 		character.x = CONFIG_WIDTH / 2 - character.width / 2;
 		character.y = CONFIG_HEIGHT / 2 - character.height / 2;
+		*/
 	}
 
 	@Override
@@ -69,7 +69,9 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 
 		// Tell the camera to update its matrices.
 		camera.update();
+		stage.draw();
 
+		/*
 		// Begin a new batch. Draw character image.
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
@@ -78,6 +80,8 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
         batch.draw(animation.getKeyFrame(elapsedTime, true), character.x, character.y);
 		batch.end();
 
+        */
+		/*
 		// Process user input.
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			character.x -= 200 * Gdx.graphics.getDeltaTime();
@@ -92,7 +96,7 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 			character.y += 200 * Gdx.graphics.getDeltaTime();
 		}
 
-		// make sure the bucket stays within the screen bounds
+		// make sure the character stays within the screen bounds
 		if (character.x < 0) {
 			// Check left out of bounds
 			character.x = 0;
@@ -109,12 +113,11 @@ public class TradersOfTheLastCarp extends ApplicationAdapter {
 			// Check top out of bounds
         	character.y = CONFIG_HEIGHT - character.height;
 		}
+		*/
 	}
 	
 	@Override
 	public void dispose() {
-		batch.dispose();
-		characterImg.dispose();
-        dummyTextureAtlas.dispose();
+		stage.dispose();
 	}
 }
