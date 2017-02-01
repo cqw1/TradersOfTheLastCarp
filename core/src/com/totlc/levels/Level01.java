b9ae041259ece39fc55de927ff8cfff9800916e1
cqw1 <cqw1@rice.edu>
2017-02-01 13:35:42 -0600

    added column of triggers in level 1

--- core/src/com/totlc/levels/Level01.java
+++ core/src/com/totlc/levels/Level01.java
@@ -32,14 +32,22 @@ public class Level01 extends ALevel{
         //REQUIRED
         setPlayer(player);
 
-        ButtonTrigger tutorialButton = new ButtonTrigger(getAssetManager(), new Rectangle(TradersOfTheLastCarp.CONFIG_WIDTH / 4, TradersOfTheLastCarp.CONFIG_HEIGHT / 2, 32, TradersOfTheLastCarp.CONFIG_HEIGHT));
-        tutorialButton.moveHitBox(0,-1 * TradersOfTheLastCarp.CONFIG_HEIGHT / 2);
-
         ArrowTrap arrowTrap = new ArrowTrap(getAssetManager(), TradersOfTheLastCarp.CONFIG_WIDTH / 2, TradersOfTheLastCarp.CONFIG_HEIGHT / 2, 650);
         arrowTrap.moveRel(-arrowTrap.getWidth() / 2, -arrowTrap.getHeight() / 2);
 
-        tutorialButton.addTrap(arrowTrap);
-        addActor(tutorialButton);
+        for (int i = 0; i < TradersOfTheLastCarp.CONFIG_HEIGHT / 128; i++) {
+            ButtonTrigger trigger = new ButtonTrigger(getAssetManager(), new Rectangle(TradersOfTheLastCarp.CONFIG_WIDTH / 4, i * 128, 32, 32));
+            trigger.moveRel(-trigger.getWidth() / 2, -trigger.getHeight() / 2);
+            trigger.addTrap(arrowTrap);
+            addActor(trigger);
+        }
+
+//        ButtonTrigger tutorialButton = new ButtonTrigger(getAssetManager(), new Rectangle(TradersOfTheLastCarp.CONFIG_WIDTH / 4, TradersOfTheLastCarp.CONFIG_HEIGHT / 2, 32, TradersOfTheLastCarp.CONFIG_HEIGHT));
+//        tutorialButton.moveHitBox(0,-1 * TradersOfTheLastCarp.CONFIG_HEIGHT / 2);
+
+
+//        tutorialButton.addTrap(arrowTrap);
+//        addActor(tutorialButton);
         addActor(arrowTrap);
         
         AEnemy dummyFlan = new JustDessert(getAssetManager(),
