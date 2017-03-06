a54a25efcabb820e695bed3e0b19c3a812691013
bentubox <b2t848r8o@gmail.com>
2017-03-05 23:54:15 -0600

    Added side sprites and classes for new player characters.

---
+++ core/src/com/totlc/Actors/players/Colorado.java
@@ -0,0 +1,11 @@
+package com.totlc.Actors.players;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.totlc.Actors.Player;
+
+public class Colorado extends Player {
+
+    public Colorado(AssetManager assetManager, float x, float y) {
+        super(assetManager, x, y);
+    }
+}
---
+++ core/src/com/totlc/Actors/players/Louisiana.java
@@ -0,0 +1,11 @@
+package com.totlc.Actors.players;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.totlc.Actors.Player;
+
+public class Louisiana extends Player {
+
+    public Louisiana(AssetManager assetManager, float x, float y) {
+        super(assetManager, x, y);
+    }
+}
