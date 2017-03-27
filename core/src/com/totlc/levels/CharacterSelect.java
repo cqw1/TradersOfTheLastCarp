0bf3fdeb5834c45a4cad2c6f04746590d1aaeadd
Bao Vu <bnv1@rice.edu>
2017-03-27 17:01:18 -0500

    Fixed a memory leak--oops--and now select at the character select selects the character. You must press escape to go back.

--- core/src/com/totlc/levels/CharacterSelect.java
+++ core/src/com/totlc/levels/CharacterSelect.java
@@ -40,6 +40,7 @@ public class CharacterSelect extends ALevel{
     ButtonPrompt cursor, names;
     private float cursorScale = 0.5f;
     TotlcObject characterHover;
+    TotlcObject characterSelected;
 
     public CharacterSelect(AssetManager assetManager) {
         super(assetManager);
@@ -72,28 +73,10 @@ public class CharacterSelect extends ALevel{
                 currentlySelected = i;
 
                 // draw the character that is currently selected
-                addActor(new TotlcObject(assetManager, new Rectangle(adjustedStart, (float) characterSelectStart.getY(), 512, 512)) {
+                characterSelected = new TotlcObject(assetManager, new Rectangle(adjustedStart, (float) characterSelectStart.getY(), 512, 512)) {
                     @Override
                     public void draw(Batch batch, float alpha) {
-                        batch.draw(new Texture(Gdx.files.internal(AssetList.GREEN_SELECT_BORDER.toString())), getX(), getY(), 0, 0, 128, 128, 1f, 1f, 0, 0, 0, (int) getWidth(), (int) getHeight(), false, false);
-                    }
-
-                    @Override
-                    public boolean collidesWith(Actor otherActor) {
-                        return false;
-                    }
-
-                    @Override
-                    public void endCollidesWith(Actor otherActor) {
-
-                    }
-                });
-
-                // draw the hover
-                characterHover = new TotlcObject(assetManager, new Rectangle(adjustedStart, (float) characterSelectStart.getY(), 512, 512)) {
-                    @Override
-                    public void draw(Batch batch, float alpha) {
-                        batch.draw(new Texture(Gdx.files.internal(AssetList.GREEN_SELECT.toString())), getX(), getY(), 0, 0, 128, 128, 1f, 1f, 0, 0, 0, (int) getWidth(), (int) getHeight(), false, false);
+                        batch.draw(getTexture(), getX(), getY(), 0, 0, 128, 128, 1f, 1f, 0, 0, 0, (int) getWidth(), (int) getHeight(), false, false);
                     }
 
                     @Override
@@ -106,6 +89,28 @@ public class CharacterSelect extends ALevel{
 
                     }
                 };
+                characterSelected.setTexture(new Texture(Gdx.files.internal(AssetList.GREEN_SELECT_BORDER.toString())));
+                addActor(characterSelected);
+                characterSelected.setZIndex(2);
+
+                // draw the hover
+                characterHover = new TotlcObject(assetManager, new Rectangle(adjustedStart, (float) characterSelectStart.getY(), 512, 512)) {
+                    @Override
+                    public void draw(Batch batch, float alpha) {
+                        batch.draw(getTexture(), getX(), getY(), 0, 0, 128, 128, 1f, 1f, 0, 0, 0, (int) getWidth(), (int) getHeight(), false, false);
+                    }
+
+                    @Override
+                    public boolean collidesWith(Actor otherActor) {
+                        return false;
+                    }
+
+                    @Override
+                    public void endCollidesWith(Actor otherActor) {
+
+                    }
+                };
+                characterHover.setTexture(new Texture(Gdx.files.internal(AssetList.GREEN_SELECT.toString())));
                 addActor(characterHover);
                 characterHover.setZIndex(2);
             }
@@ -175,8 +180,7 @@ public class CharacterSelect extends ALevel{
         if (keyCode == Input.Keys.LEFT) {
             currentCharacters.get(currentlySelected).setMovingDown(false);
             currentlySelected = (currentlySelected - 1 + playableCharacters) % playableCharacters;
-            Point2D.Float newLocation = characterGridPoints.get(currentlySelected);
-            characterHover.moveAbs((float) newLocation.getX(), (float) newLocation.getY());
+            characterHover.moveAbs((float) characterGridPoints.get(currentlySelected).getX(), (float) characterGridPoints.get(currentlySelected).getY());
             isHandled = true;
             Sound sound = Gdx.audio.newSound(Gdx.files.internal("sounds/UI_SFX_Set/switch7.wav"));
             sound.play(1.0f);
@@ -186,8 +190,7 @@ public class CharacterSelect extends ALevel{
         if (keyCode == Input.Keys.RIGHT) {
             currentCharacters.get(currentlySelected).setMovingDown(false);
             currentlySelected = (currentlySelected + 1) % playableCharacters;
-            Point2D.Float newLocation = characterGridPoints.get(currentlySelected);
-            characterHover.moveAbs((float) newLocation.getX(), (float) newLocation.getY());
+            characterHover.moveAbs((float) characterGridPoints.get(currentlySelected).getX(), (float) characterGridPoints.get(currentlySelected).getY());
             isHandled = true;
             Sound sound = Gdx.audio.newSound(Gdx.files.internal("sounds/UI_SFX_Set/switch7.wav"));
             sound.play(1.0f);
@@ -196,13 +199,12 @@ public class CharacterSelect extends ALevel{
 
         if (keyCode == Input.Keys.SPACE) {
             TradersOfTheLastCarp.playerClass = availablePlayers.get(currentlySelected);
-            setNextLevel(TitleScreen.class);
-            initNextLevel();
+            characterSelected.moveAbs((float) characterGridPoints.get(currentlySelected).getX(), (float) characterGridPoints.get(currentlySelected).getY());
         }
 
         if (keyCode == Input.Keys.ESCAPE) {
-            ALevel nextLevelObject = LevelFactory.createLevel(TitleScreen.class, getAssetManager());
-            loadLevel(nextLevelObject);
+            setNextLevel(TitleScreen.class);
+            initNextLevel();
             Sound sound = Gdx.audio.newSound(Gdx.files.internal("sounds/negative0.wav"));
             sound.play(1.0f);
         }
