a2667057b1d0523c46503add56667711eecbbd9c
cqw1 <cqw1@rice.edu>
2017-04-06 11:58:49 -0500

    moved each info into its own class to abstract out the x,y positions

--- core/src/com/totlc/Actors/UI/TutorialInfo.java
+++ core/src/com/totlc/Actors/UI/TutorialInfo.java
@@ -14,38 +14,56 @@ import static com.totlc.TradersOfTheLastCarp.CONFIG_WIDTH;
 
 public class TutorialInfo extends Actor {
 
-    private BitmapFont font;
-    private AssetManager assetManager;
-    private TextureAtlas arrowKeys;
-    private Texture arrow;
-    private float arrowKeyScale = 0.5f;
-    private float arrowScale = 0.5f;
-    private int arrowKeyWidth;
-    private int arrowKeyHeight;
-
-    private int verticalPadding = 20;
-    private int horizontalPadding = 5;
+    private BitmapFont font = TradersOfTheLastCarp.systemFont0;
+    private MoveInfo moveInfo;
+    private WhipInfo whipInfo;
+    private ObjectiveInfo objectiveInfo;
 
     public TutorialInfo(AssetManager assetManager) {
-        font = TradersOfTheLastCarp.systemFont0;
         font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        this.assetManager = assetManager;
 
-        arrow = assetManager.get(AssetList.ARROW.toString(), Texture.class);
+        moveInfo = new MoveInfo(assetManager, font, 1.2f, CONFIG_WIDTH / 4, CONFIG_HEIGHT * 7/8);
+        whipInfo = new WhipInfo(assetManager, font, 1.2f, CONFIG_WIDTH / 2, CONFIG_HEIGHT * 7/8);
+        objectiveInfo = new ObjectiveInfo(assetManager, font, 1.2f, CONFIG_WIDTH * 3/4, CONFIG_HEIGHT * 7/8);
+    }
+
+    @Override
+    public void draw(Batch batch, float alpha) {
+        moveInfo.draw(batch);
+        whipInfo.draw(batch);
+        objectiveInfo.draw(batch);
+    }
+}
+
+class MoveInfo {
+    private BitmapFont font;
+    private TextureAtlas arrowKeys;
+    private int arrowKeyWidth;
+    private int arrowKeyHeight;
+    private float arrowKeyScale = 0.5f;
+
+    private int verticalPadding = 130;
+    private int horizontalPadding = 5;
+    private float fontScale;
+    private float x;
+    private float y;
+
+    public MoveInfo(AssetManager assetManager, BitmapFont font, float fontScale, float x, float y) {
+        this.font = font;
+        this.fontScale = fontScale;
+        this.x = x;
+        this.y = y;
+
         arrowKeys = assetManager.get(AssetList.ARROW_KEYS.toString(), TextureAtlas.class);
         arrowKeyWidth = arrowKeys.getRegions().get(0).originalWidth;
         arrowKeyHeight = arrowKeys.getRegions().get(0).originalHeight;
     }
 
-    @Override
-    public void draw(Batch batch, float alpha) {
-        font.getData().setScale(1.2f);
-        font.draw(batch, "MOVE", CONFIG_WIDTH / 4 + (arrowKeyWidth) * arrowKeyScale + horizontalPadding, CONFIG_HEIGHT * 3/4 - verticalPadding);
-
+    public void draw(Batch batch) {
         batch.draw(
                 arrowKeys.findRegion("key_left"),
-                CONFIG_WIDTH / 4,
-                CONFIG_HEIGHT * 7/8 - (arrowKeys.getRegions().get(0).originalHeight),
+                x,
+                y - (arrowKeys.getRegions().get(0).originalHeight),
                 0,
                 0,
                 arrowKeyWidth,
@@ -57,8 +75,8 @@ public class TutorialInfo extends Actor {
 
         batch.draw(
                 arrowKeys.findRegion("key_up"),
-                CONFIG_WIDTH / 4 + 1 + arrowKeyWidth * arrowKeyScale,
-                CONFIG_HEIGHT * 7/8 - (arrowKeys.getRegions().get(0).originalHeight  + 10) + arrowKeyHeight * arrowKeyScale,
+                x + 1 + arrowKeyWidth * arrowKeyScale,
+                y - (arrowKeys.getRegions().get(0).originalHeight  + 10) + arrowKeyHeight * arrowKeyScale,
                 0,
                 0,
                 arrowKeyWidth,
@@ -70,8 +88,8 @@ public class TutorialInfo extends Actor {
 
         batch.draw(
                 arrowKeys.findRegion("key_bottom"),
-                CONFIG_WIDTH / 4 + arrowKeyWidth * arrowKeyScale,
-                CONFIG_HEIGHT * 7/8 - (arrowKeys.getRegions().get(0).originalHeight),
+                x + arrowKeyWidth * arrowKeyScale,
+                y - (arrowKeys.getRegions().get(0).originalHeight),
                 0,
                 0,
                 arrowKeyWidth,
@@ -83,8 +101,8 @@ public class TutorialInfo extends Actor {
 
         batch.draw(
                 arrowKeys.findRegion("key_right"),
-                CONFIG_WIDTH / 4 + (2 * arrowKeyWidth * arrowKeyScale),
-                CONFIG_HEIGHT * 7/8 - (arrowKeys.getRegions().get(0).originalHeight),
+                x + (2 * arrowKeyWidth * arrowKeyScale),
+                y - (arrowKeys.getRegions().get(0).originalHeight),
                 0,
                 0,
                 arrowKeyWidth,
@@ -94,10 +112,42 @@ public class TutorialInfo extends Actor {
                 0
         );
 
+        font.getData().setScale(fontScale);
+        font.draw(batch, "MOVE", x - 2 + (arrowKeyWidth) * arrowKeyScale + horizontalPadding, y - verticalPadding);
+
+    }
+
+}
+
+class WhipInfo {
+    private BitmapFont font;
+    private TextureAtlas arrowKeys;
+    private int arrowKeyWidth;
+    private int arrowKeyHeight;
+    private float arrowKeyScale = 0.5f;
+
+    private int verticalPadding = 130;
+    private int horizontalPadding = 5;
+    private float fontScale;
+    private float x;
+    private float y;
+
+    public WhipInfo(AssetManager assetManager, BitmapFont font, float fontScale, float x, float y) {
+        this.font = font;
+        this.fontScale = fontScale;
+        this.x = x;
+        this.y = y;
+
+        arrowKeys = assetManager.get(AssetList.ARROW_KEYS.toString(), TextureAtlas.class);
+        arrowKeyWidth = arrowKeys.getRegions().get(0).originalWidth;
+        arrowKeyHeight = arrowKeys.getRegions().get(0).originalHeight;
+    }
+
+    public void draw(Batch batch) {
         batch.draw(
                 arrowKeys.findRegion("space"),
-                CONFIG_WIDTH / 2 + 50,
-                CONFIG_HEIGHT * 7/8  - (arrowKeys.getRegions().get(0).originalHeight),
+                x + 50,
+                y - (arrowKeys.getRegions().get(0).originalHeight),
                 0,
                 0,
                 arrowKeyWidth,
@@ -106,13 +156,38 @@ public class TutorialInfo extends Actor {
                 arrowKeyScale,
                 0
         );
-        font.getData().setScale(1.2f);
-        font.draw(batch, "WHIP", CONFIG_WIDTH / 2 + 50 + horizontalPadding, CONFIG_HEIGHT * 3/4 - verticalPadding);
 
+        font.getData().setScale(fontScale);
+        font.draw(batch, "WHIP", x + 50 + horizontalPadding, y - verticalPadding);
+
+    }
+
+}
+
+class ObjectiveInfo {
+    private BitmapFont font;
+    private Texture arrow;
+    private float arrowScale = 0.5f;
+    private int verticalPadding = 130;
+
+    private float fontScale;
+    private float x;
+    private float y;
+
+    public ObjectiveInfo(AssetManager assetManager, BitmapFont font, float fontScale, float x, float y) {
+        this.font = font;
+        this.fontScale = fontScale;
+        this.x = x;
+        this.y = y;
+
+        arrow = assetManager.get(AssetList.ARROW.toString(), Texture.class);
+    }
+
+    public void draw(Batch batch) {
         batch.draw(
                 arrow,
-                CONFIG_WIDTH * 3/4 + arrow.getWidth() / 4,
-                CONFIG_HEIGHT * 7/8 - arrow.getHeight() / 2,
+                x + arrow.getWidth() / 4,
+                y - arrow.getHeight() / 2,
                 0,
                 0,
                 arrow.getWidth(),
@@ -128,8 +203,9 @@ public class TutorialInfo extends Actor {
                 false
         );
 
-        font.getData().setScale(1.2f);
-        font.draw(batch, "CHECK LEVEL OBJECTIVE", CONFIG_WIDTH * 3/4, CONFIG_HEIGHT * 3/4 - verticalPadding);
-    }
-}
+        font.getData().setScale(fontScale);
+        font.draw(batch, "CHECK LEVEL OBJECTIVE", x, y - verticalPadding);
 
+    }
+
+}
